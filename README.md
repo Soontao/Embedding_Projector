c13638331f29690358c0426646d1b4fd6cf23f57
Theo Sun <theo.sun.github@outlook.com>
2023-12-09 21:01:57 +0800

    feat: v1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Embedding Projector (standalone app of tensorboard)
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: './src/bundle.ts',
+	mode: "development",
+	output: {
+		filename: 'standalone_bundle.js',
+		path: path.join(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: path.join(__dirname, 'public/index.html'), to: 'index.html' },
+				{ from: path.join(__dirname, 'public/standalone_projector_config.json'), to: 'standalone_projector_config.json' },
+			],
+		}),
+	],
+	resolve: {
+		extensions: ['.ts', '.js'],
+	}
+};
